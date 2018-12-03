5f18892e8020aa5735d6d4035678732c5009cb25
Mark Sliva <marksliva8@gmail.com>
2018-12-02 23:04:51 -0700

    run resnet in job

--- train_script.sh
+++ train_script.sh
@@ -1,8 +1,6 @@
 #!/bin/bash
 
-git checkout resnet152
-
-python -u train.py -n "resnet_152" -d "hpa" --train-images-path="/hpakf-image-data/data/train_images" --test-images-path="/hpakf-image-data/data/test_images" --nEpochs=1 --batchSz=256  --use-cuda=yes
+python -u train.py -n "resnet_152" -d "hpa" --train-images-path="/hpakf-image-data/data/train_images" --test-images-path="/hpakf-image-data/data/test_images" --nEpochs=1 --batchSz=8  --use-cuda=yes
 
 echo "*** training complete. pod will stop in 10 minutes ***"
 
